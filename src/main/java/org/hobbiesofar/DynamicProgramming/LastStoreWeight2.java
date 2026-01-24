03a100f8909ae5508c88389d8a0122bb6b009026
hobbyist-programmer-ar <hobbyistprogrammer@outlook.com>
2026-01-24 22:14:37 +0530

    Leet Code : Last Stone Weight II

---
+++ src/main/java/org/hobbiesofar/DynamicProgramming/LastStoreWeight2.java
@@ -0,0 +1,30 @@
+package org.hobbiesofar.DynamicProgramming;
+
+/*
+
+*/
+
+class LastStoreWeight2 {
+    public int lastStoneWeightII(int[] stones) {
+        int totalSum = 0;
+        for(int stone : stones) {
+            totalSum += stone;
+        }
+
+        int numStone = stones.length;
+        int targetCapacity = totalSum / 2;
+
+        int[][] dp = new int [numStone + 1][targetCapacity + 1];
+
+        for(int i = 1; i <= numStone; i++) {
+            for(int j = 0; j <= targetCapacity; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if(stones[i - 1] <= j) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - stones[i - 1]] + stones[i - 1]);
+                }
+            }
+        }
+
+        return totalSum - dp[numStone][targetCapacity] * 2;
+    }
+}
